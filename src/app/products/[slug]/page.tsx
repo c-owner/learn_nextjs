46a7443657e19a7c1bdc92efaf4f87cb67f7ba67
corner <corner3499@gmail.com>
2023-09-03 11:19:57 +0900

    SSG, ISR, SSR 요청 주기 옵션

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -13,7 +13,7 @@ export function generateMetadata({ params }: Props) {
     }
 }
 
-export const revalidate = 3;
+// export const revalidate = 3;
 export default async function ProductPage({ params: {slug} }: Props) {
     const product = await getProduct(slug);
 
