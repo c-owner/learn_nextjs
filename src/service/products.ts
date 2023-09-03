15f8b9b3de753345179db2d9ef9c327b18f37437
corner <corner3499@gmail.com>
2023-09-03 10:50:05 +0900

    SSG Rendered

--- src/service/products.ts
+++ src/service/products.ts
@@ -1,7 +1,21 @@
-export function getProducts() {
-    return ['shirt', 'pants', 'shoes', 'bags', 'dress'];
+import path from "node:path";
+import {promises as fs} from "fs";
+
+
+export type Product = {
+    id: string;
+    name: string;
+    price: number;
+};
+
+export async function getProducts(): Promise<Product[]> {
+    const filePath = path.join(process.cwd(), 'data', 'products.json');
+    // promise 이기 떄문에 async await 비동기 데이터 사용
+    const data = await fs.readFile(filePath, 'utf-8');
+    return JSON.parse(data);
 }
 
-export function getProduct(id: string) {
-    return id;
+export async function getProduct(id: string): Promise<Product | undefined> {
+    const products = await getProducts();
+    return products.find((product) => product.id === id);
 }
