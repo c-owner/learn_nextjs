0676c47b189a1139ad57753e302931828388c6b7
corner <corner3499@gmail.com>
2023-09-03 10:28:43 +0900

    refactor

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import {notFound} from "next/navigation";
+import {getProduct, getProducts} from "@/service/products";
 
 type Props = {
     params: {
@@ -12,16 +13,20 @@ export function generateMetadata({ params }: Props) {
     }
 }
 
-export default function PantsPage({params}: Props) {
-    if(params.slug === 'nothing') {
-        notFound();
+export default function PantsPage({ params: {slug} }: Props) {
+    const product = getProduct(slug);
+
+    if (!product) {
+        return notFound();
     }
-    return <h2>{params.slug} 제품 설명 페이지</h2>;
+    // 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
+    return <h2>{product} 제품 설명 페이지</h2>;
 }
 
 export function generateStaticParams() {
-    const products = ['pants', 'skirt'];
-    return products.map(product => ({
+    // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임 (SSG)
+    const products = getProducts();
+    return products.map(product  => ({
         slug: product,
     }));
 }
