37f31726e8d82e87125f5d45eb0b1b0dab170a5d
corner <corner3499@gmail.com>
2023-09-04 10:29:13 +0900

    local Image, remote Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,18 @@
 // 서버 컴포넌트는 서버에서만 실행된다.
 // 서버 컴포넌트는 브라우저에서 실행되지 않기 때문에 브라우저에서 사용하는 객체를 사용할 수 없다. e.g) 상태관리 등
 import os from 'os';
-import Counter from "@/components/Counter"; // 노드 APIs
+import Counter from "@/components/Counter";
+import Image from "next/image"; // 노드 APIs
 export default function Home() {
-    console.log("안녕");
-    console.log(os.hostname());
 
     return (
         <>
             <h1>홈페이지</h1>
             <Counter />
+            {/* 이미지에서 외부 경로를 사용할 땐 width와 height를 사용하여야 한다. */}
+            <Image src="https://images.unsplash.com/photo-1441986300917-64674bd600d8"
+                   width={400} height={400}
+                   alt="shop" />
         </>
 
     );
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import {getProducts} from "@/service/products";
 import MeowArticle from "@/components/MeowArticle";
+import Image from "next/image";
+import clothesImage from "../../../assets/images/clothes.jpg";
 
 // export const revalidate = 3;
 export default async function ProductsPage() {
@@ -11,6 +13,7 @@ export default async function ProductsPage() {
     return (
         <>
             <h1>제품 소개 페이지</h1>
+            <Image src={clothesImage} alt={"clothes"}/>
             <ul>
                 {products.map(({id, name}, index) => (
                     <li key={index}>
